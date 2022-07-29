050b611dc731877682ae08ec7427ef36d0c0aac6
Tony Bermea <32187575+TonyBermea@users.noreply.github.com>
2022-07-29 21:53:36 +0000

    Markdown Editor - DONE

---
+++ markdown-editor/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import LiveMarkdown from './components/LiveMarkdown';
+
+function App() {
+  return (
+    <div>
+     <LiveMarkdown />
+    </div>
+  );
+}
+
+export default App;
